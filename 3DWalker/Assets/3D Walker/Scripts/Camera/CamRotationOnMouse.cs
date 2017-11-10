8513fdde1908ef2de36ea7db693aabb3581098a0
Muhammad Faizan Khan <lovingfkj@yahoo.com>
2017-11-10 18:44:08 +0500

    Cam Fly rotation on Mouse Left click added
    CamRotationOnMouse script has added in project. It has attached to Cam Fly so that cam fly move on mouse left click drag.

---
+++ 3DWalker/Assets/3D Walker/Scripts/Camera/CamRotationOnMouse.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace ThreeDWalker
+{
+    /// <summary>
+    /// Function: This script will use to Rotate the camera using mouse
+    /// Author: Muhammad Faizan Khan
+    /// </summary>
+    public class CamRotationOnMouse : MonoBehaviour
+    {
+        private Transform camera;
+        float yaw = 0.0f; // Rotation around the vertical axis is called yaw
+        float pitch = 0.0f;//Rotation around the side-to-side axis is called pitch
+
+        public float rotationSpeed = 2f;
+
+        void Start()
+        {
+            camera = this.transform;
+        }
+
+        void Update()
+        {
+            if (Input.GetMouseButton(1))
+            {
+                yaw += Input.GetAxis("Mouse X") * Time.deltaTime * rotationSpeed;
+                pitch -= Input.GetAxis("Mouse Y") * Time.deltaTime * rotationSpeed;
+                camera.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+            }
+        }
+
+
+    }
+}
